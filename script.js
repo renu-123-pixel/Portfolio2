342b3339cefbca2e095f14079da26b20a24437df
Renu Ekka <renuekka87836@gmail.com>
2025-11-04 16:27:44 +0530

    added some more projects

--- script.js
+++ script.js
@@ -57,8 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const message = document.getElementById("right-message");
    const container_proj = document.querySelector(".container2");
 
-    container.addEventListener("mousemove", function(e) {
-    const bounds = container.getBoundingClientRect();
+    container_proj.addEventListener("mousemove", function(e) {
+    const bounds = container_proj.getBoundingClientRect();
     const distanceFromRight = bounds.right - e.clientX;
 
     if (distanceFromRight < 50) {
@@ -68,26 +68,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  container_proj.addEventListener("mouseleave", function() {
-    message.style.display = "none";
-  });
+  const container = document.getElementById("project1");
 
-    container_proj.getElementById("project1").addEventListener("click", function (e) {
-    
-    
+  container.addEventListener("click", function (e) {
+    console.log("Clicked!");
     window.open("https://github.com/renu-123-pixel/Movie-app", "_blank");
   });
 
-  container_proj.getElementById("project2").addEventListener("click", function (e) {
-    
-    
+ const container1 = document.getElementById("project2");
+  container1.addEventListener("click", function (e) {
+    console.log("Clicked!");
     window.open("https://github.com/renu-123-pixel/RealTimeEdgeDetection.git", "_blank");
   });
-
-  container_proj.getElementById("project3").addEventListener("click", function (e) {
- 
-    
-    window.open("https://github.com/renu-123-pixel/GuessTheNumber-android-.git", "_blank");
+const container2=document.getElementById("project3");
+  container2.addEventListener("click", function (e) {
+ console.log("Clicked!");
+  window.open("https://github.com/renu-123-pixel/GuessTheNumber-android-.git", "_blank");
   });
 
 const faders = document.querySelectorAll('.myProject');
@@ -247,8 +243,7 @@ document.querySelectorAll('.skill-item').forEach(item => {
                     }
                 }
             });
-        });
-    </script>
+        });  
 
 });
 
